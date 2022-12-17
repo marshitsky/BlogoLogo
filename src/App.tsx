cfba05ff9c59708432fdaecf774dd98441f83bf9
marshitsky <marshitsky@gmail.com>
2022-12-17 14:48:42 +0300

    feat(app): add router to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { AppWrapper, Par, StyledBTN } from "./ui/base";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
 
 export const App = () => {
-  const [theme, setTheme] = useState("light");
-
-  const handleTheme = () => {
-    setTheme((theme) => (theme === "dark" ? "light" : "dark"));
-  };
-
-  useEffect(() => {
-    document.documentElement.setAttribute("theme", theme);
-  }, [theme]);
-
-  return (
-    <AppWrapper>
-      <Par>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sint sed tempore ducimus
-        accusamus quia voluptate laboriosam aliquam dicta culpa.
-      </Par>
-      <StyledBTN onClick={handleTheme}>Push me</StyledBTN>
-    </AppWrapper>
-  );
+  return <RouterProvider router={router} />;
 };
