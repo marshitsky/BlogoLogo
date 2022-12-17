5ae23e7d035e1cf7f69a8a7b6260deab6cf0c62d
marshitsky <marshitsky@gmail.com>
2022-12-17 14:46:52 +0300

    feat(pages): add pages

---
+++ src/pages/ArticlePage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ArticlePage = () => {
+  return <div>ArticlePage</div>;
+};
---
+++ src/pages/index.ts
@@ -0,0 +1,19 @@
+import { AccountPage } from "./AccountPage";
+import { ArticlePage } from "./ArticlePage";
+import { FavoritesPage } from "./FavoritesPage";
+import { HomePage } from "./HomePage";
+import { NotFoundPage } from "./NotFoundPage";
+import { SearchPage } from "./SearchPage";
+import { SignInPage } from "./SignInPage";
+import { SignUpPage } from "./SignUpPage";
+
+export {
+  AccountPage,
+  ArticlePage,
+  FavoritesPage,
+  HomePage,
+  NotFoundPage,
+  SearchPage,
+  SignInPage,
+  SignUpPage,
+};
