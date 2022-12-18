2b8c04d663779c957ecd6d3a9d15498a05ef3641
marshitsky <marshitsky@gmail.com>
2022-12-18 12:54:50 +0300

    feature(components): put user-account inside nav-bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { UserAccount } from "../UserAccount/UserAccount";
 import { Navigation } from "./styles";
+import { StyledLogo, Text } from "./styles";
 
 export const NavBar = () => {
-  return <Navigation>NavBar</Navigation>;
+  return (
+    <Navigation>
+      <StyledLogo />
+      <Text>NAV BAR</Text>
+      <UserAccount />
+    </Navigation>
+  );
 };
--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -1,7 +1,23 @@
 import styled from "styled-components";
 import { Color } from "../../ui/colors";
+import { ReactComponent as Logo } from "../../assets/img/logo.svg";
+import { H2 } from "../../ui/typography";
 
-export const Navigation = styled.div`
+const Navigation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   height: 96px;
+  padding: 0 5px;
   background-color: ${Color.WHITE_HEADER};
 `;
+
+const Text = styled.p`
+  ${H2}
+`;
+
+const StyledLogo = styled(Logo)`
+  width: 157px;
+`;
+
+export { Navigation, StyledLogo, Text };
