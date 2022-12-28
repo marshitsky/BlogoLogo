99e93b5889984cd9a2fc2cdf554f6bddc8dfb2a4
marshitsky <marshitsky@gmail.com>
2022-12-28 21:19:15 +0300

    refactor(types): add index file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,20 +1,3 @@
-interface IArticle {
-  id: number;
-  title: string;
-  url: string;
-  imageUrl: string;
-  newsSite: string;
-  summary: string;
-  publishedAt: string;
-  updatedAt: string;
-  featured: boolean;
-  launches: ILaunch[];
-  events: any;
-}
+import { IArticle, INews } from "./types";
 
-interface ILaunch {
-  id: string;
-  provider: string;
-}
-
-export type { IArticle };
+export type { IArticle, INews };
---
+++ src/types/types.ts
@@ -0,0 +1,34 @@
+interface IArticle {
+  id: string;
+  title: string;
+  url: string;
+  imageUrl: string;
+  newsSite: string;
+  summary: string;
+  publishedAt: string;
+  updatedAt: string;
+  featured: boolean;
+  launches: ILaunch[];
+  events: any;
+}
+
+interface INews {
+  id: string;
+  title: string;
+  url: string;
+  imageUrl: string;
+  newsSite: string;
+  summary: string;
+  publishedAt: string;
+  updatedAt: string;
+  featured: boolean;
+  launches: ILaunch[];
+  events: any;
+}
+
+interface ILaunch {
+  id: string;
+  provider: string;
+}
+
+export type { IArticle, INews };
