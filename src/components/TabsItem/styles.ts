4cf44a4ccb694032d84ad1eb6e14d49c94beed8f
marshitsky <marshitsky@gmail.com>
2022-12-29 05:41:10 +0300

    feat(tabs): add styles to tabs

--- src/components/TabsItem/styles.ts
+++ src/components/TabsItem/styles.ts
@@ -1,7 +1,19 @@
 import styled from "styled-components";
+import { Color, S1 } from "ui";
 
 const Tab = styled.button`
-  padding: 10px 25px;
+  padding: 10px 30px;
+  ${S1}
+  border: 1px solid ${Color.WHITE_HEADER};
+  border-radius: 6px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset,
+    rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset;
+  transition: 1s;
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px,
+      rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px,
+      rgba(0, 0, 0, 0.09) 0px -3px 5px;
+  }
 `;
 
 export { Tab };
