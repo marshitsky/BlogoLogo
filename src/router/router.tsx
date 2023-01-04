437b5df49ed37b249a0d20d97e043c442fe4fc42
marshitsky <marshitsky@gmail.com>
2023-01-04 19:39:43 +0300

    refactor(router): replace reset-password page to private routing

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -24,12 +24,12 @@ export const Router = createBrowserRouter(
         <Route path={ROUTE.SEARCH} element={<SearchPage />} />
         <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
         <Route path={ROUTE.SIGN_UP} element={<SignUpPage />} />
-        <Route path={ROUTE.RESET_PASSWORD} element={<ResetPasswordPage />} />
         <Route path={ROUTE.NOT_FOUND} element={<NotFoundPage />} />
       </Route>
       <Route element={<RequireAuth />}>
         <Route path={ROUTE.ACCOUNT} element={<AccountPage />} />
         <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
+        <Route path={ROUTE.RESET_PASSWORD} element={<ResetPasswordPage />} />
       </Route>
     </>,
   ),
