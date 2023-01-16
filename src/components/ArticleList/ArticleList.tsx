edd538e85ab090e9370bf89d3117503af726c0af
marshitsky <marshitsky@gmail.com>
2023-01-16 04:18:35 +0300

    refactor(list): combine article and news lists in one

--- src/components/ArticleList/ArticleList.tsx
+++ src/components/ArticleList/ArticleList.tsx
@@ -1,14 +1,30 @@
-import { ArticleListItem, Spinner } from "components";
+import { ArticleListItem, NewsListItem, Spinner } from "components";
 import { useEffect } from "react";
-import { fetchArticles, getAllArticles, useAppDispatch, useAppSelector } from "store";
+import {
+  fetchArticles,
+  fetchNews,
+  getAllArticles,
+  getAllNews,
+  useAppDispatch,
+  useAppSelector,
+} from "store";
 import { ErrorMessage, StyledArticlesList, ListWrapper } from "./styles";
 
-export const ArticleList = () => {
+interface IProps {
+  tab: string;
+}
+
+export const ArticleList = ({ tab }: IProps) => {
   const dispatch = useAppDispatch();
   const { articles, error, isLoading } = useAppSelector(getAllArticles);
+  const { news } = useAppSelector(getAllNews);
 
   useEffect(() => {
-    dispatch(fetchArticles({ page: 0, value: "" }));
+    dispatch(fetchArticles({ page: 0, value: "", word: "" }));
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchNews({ page: 0, value: "", word: "" }));
   }, [dispatch]);
 
   return (
@@ -16,8 +32,10 @@ export const ArticleList = () => {
       {isLoading && <Spinner />}
       {error && <ErrorMessage />}
       <StyledArticlesList>
-        {Array.isArray(articles) &&
-          articles.map((article) => <ArticleListItem article={article} key={article.id} />)}
+        {tab === "articles"
+          ? Array.isArray(articles) &&
+            articles.map((article) => <ArticleListItem article={article} key={article.id} />)
+          : Array.isArray(news) && news.map((news) => <NewsListItem news={news} key={news.id} />)}
       </StyledArticlesList>
     </ListWrapper>
   );
